dce6ba67c7876bf72e35cae40ab3bcd84683fe8f
JP McCool <jpm.mccool@gmail.com>
2025-03-28 08:27:32 -0400

    Implemented deck creation from kana dictionary

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from PySide6.QtWidgets import (
     QPushButton,
 )
 
+import kanadict
+
 class MainWindow (QMainWindow) :
     def __init__(self) :
         super().__init__()
@@ -23,15 +25,14 @@ class MainWindow (QMainWindow) :
         self.setWindowIcon(QIcon("icon.png"))
 
         topLabel        = QLabel("Choose your kana quiz:")
-        kanaGroup       = QGroupBox("Kana")
-        hiraganaCheck   = QCheckBox("Hiragana")
-        katakanaCheck   = QCheckBox("Katakana")
-        setGroup        = QGroupBox("Sets")
-        basicCheck      = QCheckBox("Basic Kana")
-        diacriticCheck  = QCheckBox("Diacritics ( \" / º )")
-        digraphCheck    = QCheckBox("Digraphs (yōon)")
+        kanaGroup       = QGroupBox("Script")
+        self.hiraganaCheck   = QCheckBox("Hiragana")
+        self.katakanaCheck   = QCheckBox("Katakana")
+        setGroup        = QGroupBox("Include")
+        self.basicCheck      = QCheckBox("Basic kana")
+        self.diacriticCheck  = QCheckBox("Diacritics ( \" / º )")
+        self.digraphCheck    = QCheckBox("Digraphs (yōon)")
         optionsGroup    = QGroupBox("Options")
-        
         multipleRadio   = QRadioButton("Multiple choice")
         multipleSpin    = QSpinBox()
         multipleLabel   = QLabel("choices (max. 10)")
@@ -41,23 +42,26 @@ class MainWindow (QMainWindow) :
         
         # TODO: Set previously selected options, or options from saved preferences, or sensible default options, in that order
         # TODO: Add a button to let the user save the current options, but only if they make sense
-        hiraganaCheck.setChecked(True)  # This is a sensible default
-        basicCheck.setChecked(True)     # This is a sensible default        
+        self.hiraganaCheck.setChecked(True)  # This is a sensible default
+        self.basicCheck.setChecked(True)     # This is a sensible default
+        self.diacriticCheck.setChecked(True) # This is a sensible default
+        self.digraphCheck.setChecked(True)   # This is a sensible default        
         multipleRadio.setChecked(True)  # This is a sensible default
         multipleSpin.setRange(2, 10)    # There must be at least two options for the quiz to be considered multiple choice
         multipleSpin.setValue(4)        # This is a sensible default
+        startButton.clicked.connect(self.print_deck)
                 
         kanaBox = QVBoxLayout()
         kanaBox.addStretch(1)
-        kanaBox.addWidget(hiraganaCheck)
-        kanaBox.addWidget(katakanaCheck)
+        kanaBox.addWidget(self.hiraganaCheck)
+        kanaBox.addWidget(self.katakanaCheck)
         kanaGroup.setLayout(kanaBox)
         
         setBox = QVBoxLayout()
         setBox.addStretch(1)
-        setBox.addWidget(basicCheck)
-        setBox.addWidget(diacriticCheck)
-        setBox.addWidget(digraphCheck)
+        setBox.addWidget(self.basicCheck)
+        setBox.addWidget(self.diacriticCheck)
+        setBox.addWidget(self.digraphCheck)
         setGroup.setLayout(setBox)
 
         optionsBox = QVBoxLayout()
@@ -81,6 +85,27 @@ class MainWindow (QMainWindow) :
         centralWidget = QWidget()
         centralWidget.setLayout(mainLayout)
         self.setCentralWidget(centralWidget)
+    
+    def print_deck (self) :
+        hiragana  = self.hiraganaCheck.isChecked()
+        katakana  = self.katakanaCheck.isChecked()
+        basic     = self.basicCheck.isChecked()
+        diacritic = self.diacriticCheck.isChecked()
+        digraph   = self.digraphCheck.isChecked()
+        if hiragana is katakana is False :
+            print("Bad state: script not selected")
+            return
+        if basic is diacritic is digraph is False :
+            print("Bad state: character subset not selected")
+            return
+        deck = (kanadict.hiragana if hiragana else set()) | (kanadict.katakana if katakana else set())
+        if not basic :
+            deck -= kanadict.basic
+        if not diacritic :
+            deck -= kanadict.diacritic
+        if not digraph :
+            deck -= kanadict.digraph
+        print(deck)
     
 app = QApplication(sys.argv)
 
