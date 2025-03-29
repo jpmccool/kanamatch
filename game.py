cd11ad3391d8500b6e043b87846ed0e295d77965
JP McCool <jpm.mccool@gmail.com>
2025-03-29 06:01:52 -0400

    Implemented basic multiple choice quiz

--- game.py
+++ game.py
@@ -17,14 +17,17 @@ from PySide6.QtWidgets import (
 import kanadict
 
 class GameWindow (QMainWindow) :
-    def __init__(self, deck) :
+    def __init__(self, deck, isHard) :
         super().__init__()
         
-        self.setWindowTitle("Quiz Progress: 0%")
         self.setWindowIcon(QIcon("icon.png"))
         
-        self.deck = deck
-        self.tags = deck.copy()
+        self.deck = deck    # The deck of flash cards to draw
+        self.isHard = isHard
+        
+        self.score = 0
+        self.n_cards = len(self.deck)
+        
         self.cardLabel = QLabel(self)
         self.inputLayout = QHBoxLayout()
         
@@ -36,38 +39,72 @@ class GameWindow (QMainWindow) :
         centralWidget.setLayout(mainLayout)
         self.setCentralWidget(centralWidget)
         
-    def draw_card (self) :
+    def draw (self) :
+        self.setWindowTitle("Quiz Progress: " + str(round(100 * (1 - (len(self.deck) / self.n_cards)))) + "%")
         card = self.deck.pop()
         self.cardLabel.setPixmap(QPixmap("./kana/" + card + ".png"))
         return card
     
+    def end (self) : # TODO: Make a much more satisfying ending
+        print("Complete!")
+        print("Final score:", self.score)
+        self.setWindowTitle("Quiz Complete!")
+        # TODO: Replace graphic in label with a report of how the user did on their quiz
+        # TODO: The above will require a bit more data collection on successes/failures in subclasses
+        # TODO: Clear all widgets in the inputLayer, replace with a button that says "Close Results" that closes the window
+    
 
 class MultipleChoiceGameWindow (GameWindow) :
-    def __init__ (self, deck, choices, hardMode) :
-        super().__init__(deck)
-        self.draw(choices, hardMode)
+    def __init__ (self, deck, isHard, choices) :
+        super().__init__(deck, isHard)
+        self.tags = [*self.deck]
+        self.choices = choices
+        self.draw()
     
-    def draw (self, choices, hardMode) :
-        curr_card = self.draw_card()
+    def draw (self) :
+        curr_card = super().draw()
+        
+        while self.inputLayout.count() > 0 :
+            button = self.inputLayout.takeAt(0)
+            if button.widget() is True :
+                button.widget().deleteLater()
+        
+        
         self.tags.remove(curr_card)
         # TODO: also remove any tags from self.tags with conflicting labels
-        hand = random.sample(self.tags, choices - 1)
+        hand = random.sample(self.tags, self.choices - 1)
         hand.append(curr_card)
-        hand = random.sample(hand, choices)
+        hand = random.sample(hand, self.choices)
+        
         for card in hand :
-            button = QPushButton(kanadict.kana[card][1])
+            button = QPushButton(kanadict.kana[card][1]) # TODO: Set the label to include alternate acceptable answers, e.g. he / e, o / wo, etc.
             if card is curr_card :
-                button.clicked.connect(self.printOK)
+                button.clicked.connect(self.success)
             else :
-                button.clicked.connect(self.printNG)
+                button.clicked.connect(self.failure)
             self.inputLayout.addWidget(button)
-        self.tags.add(curr_card)
+        self.tags.append(curr_card)
         # TODO: Also add back in any cards taken out in the TODO above
             
-    def printOK (self) :
-        print("CORRECT!")
-    def printNG (self) :
-        print("Wrong answer!")
+    def success (self) :
+        self.score += 1
+        self.next()
+    
+    def failure (self) :
+        self.score -= 1
+        if self.isHard :
+            self.next()
+        else :
+            self.sender().setEnabled(False)
+    
+    def next (self) :
+        if len(self.deck) > 0 :
+            self.draw()
+        else : # End condition reached
+            self.end()
+    
+    
+    
 
     
 if __name__ == '__main__' :
--- main.py
+++ main.py
@@ -25,21 +25,21 @@ class MainWindow (QMainWindow) :
         self.setWindowTitle("Kana Match")
         self.setWindowIcon(QIcon("icon.png"))
 
-        topLabel        = QLabel("Choose your kana quiz:")
-        kanaGroup       = QGroupBox("Script")
+        topLabel             = QLabel("Choose your kana quiz:")
+        kanaGroup            = QGroupBox("Script")
         self.hiraganaCheck   = QCheckBox("Hiragana")
         self.katakanaCheck   = QCheckBox("Katakana")
-        setGroup        = QGroupBox("Include")
+        setGroup             = QGroupBox("Include")
         self.basicCheck      = QCheckBox("Basic kana")
         self.diacriticCheck  = QCheckBox("Diacritics ( \" / º )")
         self.digraphCheck    = QCheckBox("Digraphs (yōon)")
-        optionsGroup    = QGroupBox("Options")
-        multipleRadio   = QRadioButton("Multiple choice")
-        multipleSpin    = QSpinBox()
-        multipleLabel   = QLabel("choices (max. 10)")
-        inputRadio      = QRadioButton("Rōmaji input")
-        hardCheck       = QCheckBox("No second chances (hard)") 
-        startButton     = QPushButton("Start!")
+        optionsGroup         = QGroupBox("Options")
+        multipleRadio        = QRadioButton("Multiple choice")
+        self.multipleSpin    = QSpinBox()
+        multipleLabel        = QLabel("choices (max. 10)")
+        inputRadio           = QRadioButton("Rōmaji input")
+        self.hardCheck       = QCheckBox("No second chances (hard)") 
+        startButton          = QPushButton("Start!")
         
         # TODO: Set previously selected options, or options from saved preferences, or sensible default options, in that order
         # TODO: Add a button to let the user save the current options, but only if they make sense
@@ -48,9 +48,9 @@ class MainWindow (QMainWindow) :
         self.diacriticCheck.setChecked(False) # This is a sensible default
         self.digraphCheck.setChecked(False)   # This is a sensible default        
         multipleRadio.setChecked(True)  # This is a sensible default
-        multipleSpin.setRange(2, 10)    # There must be at least two options for the quiz to be considered multiple choice
-        multipleSpin.setValue(4)        # This is a sensible default
-        startButton.clicked.connect(self.print_deck)
+        self.multipleSpin.setRange(2, 10)    # There must be at least two options for the quiz to be considered multiple choice
+        self.multipleSpin.setValue(4)        # This is a sensible default
+        startButton.clicked.connect(self.launch_game)
                 
         kanaBox = QVBoxLayout()
         kanaBox.addStretch(1)
@@ -69,11 +69,11 @@ class MainWindow (QMainWindow) :
         optionsBox.addStretch(1)
         optionsBox.addWidget(multipleRadio)
         multipleBox = QHBoxLayout()        
-        multipleBox.addWidget(multipleSpin)
+        multipleBox.addWidget(self.multipleSpin)
         multipleBox.addWidget(multipleLabel)
         optionsBox.addLayout(multipleBox)
         optionsBox.addWidget(inputRadio)
-        optionsBox.addWidget(hardCheck)        
+        optionsBox.addWidget(self.hardCheck)        
         optionsGroup.setLayout(optionsBox)
         
         mainLayout = QVBoxLayout()
@@ -87,7 +87,7 @@ class MainWindow (QMainWindow) :
         centralWidget.setLayout(mainLayout)
         self.setCentralWidget(centralWidget)
     
-    def print_deck (self) :
+    def launch_game (self) :
         hiragana  = self.hiraganaCheck.isChecked()
         katakana  = self.katakanaCheck.isChecked()
         basic     = self.basicCheck.isChecked()
@@ -107,7 +107,7 @@ class MainWindow (QMainWindow) :
         if not digraph :
             deck -= kanadict.digraph
         
-        self.game = MultipleChoiceGameWindow(deck, 5, True)
+        self.game = MultipleChoiceGameWindow(deck, self.hardCheck.isChecked(), self.multipleSpin.value())
         self.game.show()
         
     
